8f5527f62a87e1506b7025e40c3917a65c5b2be0
Michael Crombie <mcrombie1994@gmail.com>
2020-02-12 17:25:15 -0600

    trying once more!

--- server.js
+++ server.js
@@ -7,3 +7,11 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
+
+//PATH LOCATION STRATEGY
+
+app.get('/*', function(req, res){
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
+
+console.log(`CONSOLE LISTENING!`)
