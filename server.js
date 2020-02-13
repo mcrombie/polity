38ba1e9193da9ba8544f84a46b3df127e852b2c6
Michael Crombie <mcrombie1994@gmail.com>
2020-02-12 20:42:59 -0600

    fingers crossed

--- server.js
+++ server.js
@@ -9,13 +9,16 @@ app.use(express.static(__dirname + '/dist'));
 // Heroku port
 app.listen(process.env.PORT || 8080);
 
+
+
+
 //PATH LOCATION STRATEGY
 
 app.get('/*', function(req, res){
     console.log(__dirname);
     console.log(__dirname + '/dist/index.html');
-    console.log(path.join(__dirname + '/dist/index.html'));
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    console.log(path.join(__dirname + '/dist/root-of-civilization/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/root-of-civilization/index.html'));
 })
 
 console.log(`CONSOLE LISTENING!`)
